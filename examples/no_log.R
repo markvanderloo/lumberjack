4268b3b7f50fbcc95d125262bafb69611eee73bd
Floris Ruijter <fec.ruijter@cbs.nl>
2023-03-28 16:08:17 +0200

    Added example wrt no_log

---
+++ examples/no_log.R
@@ -0,0 +1,11 @@
+
+logfile <- tempfile(fileext=".csv")
+out <- women %L>%
+  start_log(log=no_log$new(verbose=FALSE)) %L>%
+  identity() %L>%
+  head() %L>% 
+  dump_log(file=logfile, stop=TRUE)
+
+
+read.csv(logfile,stringsAsFactors=FALSE) # Empty
+
